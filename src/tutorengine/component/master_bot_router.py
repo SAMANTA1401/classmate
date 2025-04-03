49c1dc31e295e51209624ee73578281bcc5e61bb
samanta1401 <psamanta1401@gmail.com>
2025-04-04 00:44:24 +0530

    master bot added

--- src/tutorengine/component/master_bot_router.py
+++ src/tutorengine/component/master_bot_router.py
@@ -1,7 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from IPython.display import display, Markdown
-from src.tutorengine.prompts import MasterBotPrompt
+from src.tutorengine.prompts import MasterBotPrompt 
+from langchain.prompts import PromptTemplate 
+from langchain.output_parsers import PydanticOutputParser 
+from src.tutorengine.utils import FieldSelectionParser
+from src.tutorengine.search_tools import SearchToolMasterBot
 
 import os
 from dotenv import load_dotenv
@@ -30,14 +34,49 @@ class MasterBotRouter:
     def __init__(self, chat_history:list ):
         self.llm4 = ChatGroq(model="llama3-8b-8192")
         self.chat_history = chat_history
-        self.prompt = MasterBotPrompt.masterTemplate
+        self.prompttemplate= MasterBotPrompt.masterTemplate
 
-    def route(self, prompt):
+    def route(self, state):
         
+        messages =state["messages"]
+        question=messages[-1] # last message
         
+        chat_history_str = " "
+        chat_history_str = "\n".join(chat_history)
         
-        pass         
+        template = self.prompttemplate
+        parser = PydanticOutputParser(pydantic_object=FieldSelectionParser)
+         
+        prompt = PromptTemplate(template=template,
+                                input_variables=[chat_history_str, question],
+                                partial_variables={
+                                    "format_instructions" : parser.get_format_instructions()    
+                                } 
+                                )
+        chain =  prompt | self.llm4 | parser
+        
+        response = chain.invoke({"chat_history":chat_history_str,"question":question,"format_instructions" : parser.get_format_instructions() })
+    
+        searchresults = None
+    
+        if response.Answer == "I’m not aware, search needed":
+            search = SearchToolMasterBot.master_search(question=str(question))
+            searchresults = f"Hi, I looked it up and what I found: {search.extracted_result}"
+            # Update the response with search result
 
-    def display(self, prompt):
-        response = self.llm.invoke(prompt)
-        display(Markdown(response.content))
+         # Update response based on whether search was performed
+        final_response = FieldSelectionParser(
+            Field_study=response.Field_study,
+            Subject=response.Subject,
+            Chapter=response.Chapter,
+            Topic=response.Topic,
+            Difficulty_level=response.Difficulty_level,
+            Question_or_query=response.Question_or_query,
+            Answer=searchresults if searchresults else response.Answer
+        )   
+        
+        chat_history.extend([f"UserMessage:{final_response.Question_or_query}",f"SystemMessage:{final_response.Answer}"])
+        
+        return {"messages": [final_response]}
+
+    
