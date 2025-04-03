e5d5800953d5a403b5b1377e9f3c7ad862939f93
samanta1401 <psamanta1401@gmail.com>
2025-04-03 22:44:35 +0530

    logger added

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__versio__ = "0.0.0"
+__versio__ = "0.0.1"
 
 REPO_NAME = "classmate"
 AUTHOR_USER_NAME = "SAMANTA1401"
 SRC_REPO = "src"
 
 setuptools.setup(
-    name=SRC_REPO,
+    name=REPO_NAME,
     version=__versio__,
     author=AUTHOR_USER_NAME,
     description="A small python package for tutorengine",
@@ -21,6 +21,5 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages()
 )
